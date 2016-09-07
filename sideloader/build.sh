a50db83a5f943f39503417e521b82acc865d9dda
phalamag <phalamag@gmail.com>
2016-09-07 21:28:16 +0200

    Another try

--- sideloader/build.sh
+++ sideloader/build.sh
@@ -3,4 +3,4 @@
 cp -a $REPO ./build/$NAME
 
 ${PIP} install -r $REPO/nurseconnect/requirements.txt
-
+${PIP} install -e git+https://github.com/praekelt/molo.profiles.git@feature/reset_password#egg=molo.profiles
