178faad648be6105297b147aa70a38587bdea7e1
phalamag <phalamag@gmail.com>
2016-09-07 22:41:16 +0200

    Sanity check

--- sideloader/build.sh
+++ sideloader/build.sh
@@ -3,4 +3,5 @@
 cp -a $REPO ./build/$NAME
 
 ${PIP} install -r $REPO/nurseconnect/requirements.txt
+${PIP} uninstall molo.profiles
 ${PIP} install -e git+https://github.com/praekelt/molo.profiles.git@feature/reset_password#egg=molo.profiles
