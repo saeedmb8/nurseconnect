a03853da2dcdef1d8df9af1d0dbc7804657f6f63
Saeed Marzban <saeed@praekelt.org>
2016-09-21 14:57:25 +0200

    remove CAS setup

--- nurseconnect/settings/production.py
+++ nurseconnect/settings/production.py
@@ -48,23 +48,7 @@ RAVEN_CONFIG = {
 }
 
 # Setup for CAS
-ENABLE_SSO = True
-
-MIDDLEWARE_CLASSES += [
-    "molo.core.middleware.MoloCASMiddleware",
-    "molo.core.middleware.Custom403Middleware",
-]
-
-
-AUTHENTICATION_BACKENDS = [
-    "django.contrib.auth.backends.ModelBackend",
-    "molo.core.backends.MoloCASBackend",
-]
-
-CAS_SERVER_URL = environ.get('CAS_SERVER_URL') or ''
-CAS_ADMIN_PREFIX = "/admin/"
-LOGIN_URL = "/accounts/login/"
-CAS_VERSION = "3"
+ENABLE_SSO = False
 
 try:
     from .local import *  # noqa
